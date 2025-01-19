7efad9d1cd8a6e666b4a56688668e473f3c399a7
Agustín Ceballos <aceballos1923@gmail.com>
2025-01-19 09:31:48 -0300

    feat: Agrego rutas

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  url: 'http://localhost:8080',
+  prefix: '/api',
+  prefixAuth: '/auth',
+  login: '/login',
+  registro: '/registro',
+  usuarios: '/usuarios',
+  canchas: '/canchas',
+  canchasActivas: '/canchas-activas',
+  reservas: '/reservas',
+}
