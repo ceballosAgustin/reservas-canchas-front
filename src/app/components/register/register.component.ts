ccaa2ae9d5e7c947955268283cab1434fd27f3c0
Agustín Ceballos <aceballos1923@gmail.com>
2025-02-07 14:44:41 -0300

    feat: Componente registro
    Se agrega el componente de registro con su diseño y su lógica

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule, ReactiveFormsModule, RouterModule, CommonModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  registroForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private toast: ToastrService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  )
+  {
+    this.registroForm = formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      clave: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(256)]],
+      nombre: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
+      apellido: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]]
+    });
+  }
+
+  onRegistro(): void {
+    if(this.registroForm.valid) {
+      const usuarioNuevo = this.registroForm.value;
+
+      this.authService.registro(usuarioNuevo).subscribe({
+        next: () => {
+          this.toast.success("Debes iniciar sesión para continuar", "¡Registro exitoso!");
+          this.router.navigate(["/login"]);
+        },
+        error: (err) => {
+          if(err.status == 400) {
+            this.toast.error(`Hubo un problema con los datos proporcionados`, "¡Ups!");
+          }
+        }
+      });
+    } else {
+      this.toast.warning("Completa todos los datos, por favor", "¡Atención!");
+    }
+  }
+
+  goToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+}
