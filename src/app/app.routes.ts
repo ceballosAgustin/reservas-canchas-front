4983bd368afa53ea58e86609433105906f181e9c
Agustín Ceballos <aceballos1923@gmail.com>
2025-02-07 14:43:44 -0300

    feat: Agrego ruta register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/login/login.component';
 import { loginGuard } from './core/guards/login.guard';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
   {
@@ -16,6 +17,11 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [loginGuard]
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
     canActivate: [loginGuard]
   },
   {
