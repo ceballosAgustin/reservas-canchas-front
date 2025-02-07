5f1f8d2a10abe0fc5d7e93afa32c64d9d70ce023
Agustín Ceballos <aceballos1923@gmail.com>
2025-02-06 22:05:48 -0300

    feat: Agrego animaciones al config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,4 @@
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -11,6 +12,7 @@ import { JwtModule } from '@auth0/angular-jwt';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimations(),
     provideRouter(routes),
     provideToastr({ preventDuplicates: true, timeOut: 3000 }),
     provideHttpClient(withInterceptors([tokenInterceptor, authInterceptor])),
